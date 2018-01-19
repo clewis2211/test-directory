8a32db53949e604b5dcbf367bef33da75c324298
Zay19 <carltonlewis42@gmail.com>
2018-01-19 16:08:45 -0600

    text to page

--- 99Lines_Of_Code/script.js
+++ 99Lines_Of_Code/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-
+    // mySingingFriends();
 });
 
 
@@ -12,8 +12,36 @@ var line3 = ' strikes one out, clears it all out ';
 var line4 = ' lines of code in the file '; 
 var line5 = ' no more lines of code in the file.';
 
-let mySingingFriends = () =>{
-    for(let i = 0; i<friends.length; i++){
-        
+let btn = document.getElementById('click');
+
+btn.addEventListener('click', function mySingingFriends(){
+
+    for(let i = 0; i < friends.length; i++){
+        let friend = friends[i];
+        let head = document.createElement('h1');
+        let nameText = document.createTextNode(friend);
+        console.log(nameText);
+        let myDiv = document.createElement('div');
+
+        head.appendChild(nameText);
+        myDiv.appendChild(head);
+        document.body.appendChild(myDiv);
+
+        for(let j = 99; j > 0; j-- ){
+
+            let para = document.createElement('p');
+
+            if( j > 1){
+                para.innerText = `${j} ${line1} ${j} ${line2} ${nameText} ${line3} 
+                ${(j-1)} ${line4}`
+            } else {
+                para.innerText = `${j} ${line1} ${j} ${line2} ${nameText} ${line3} 
+                ${(j-1)} ${line5}`
+            }
+            myDiv.appendChild(para);
+        }
     }
-}
+});
+
+
+
